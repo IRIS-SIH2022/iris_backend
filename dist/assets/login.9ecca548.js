3571903b1c69043d303a9c4de30c6e1800e99c3a
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-08-26 09:39:06 +0530

    Add: updated site

---
+++ dist/assets/login.9ecca548.js
@@ -0,0 +1,1 @@
+import"./style.a87c9e06.js";document.getElementById("login-form").addEventListener("submit",async t=>{t.preventDefault();const e=new FormData(t.target),o=Object.fromEntries(e);console.log(o)});
